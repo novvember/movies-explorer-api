eb19eb490c74b5567dc9be05556e1e60339bf39d
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-04 23:39:40 +0300

    feat: add database connection

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
+const DATABASE_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 
 const app = express();
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => {
+    console.log(`Connected to database on ${DATABASE_URL}`);
+  })
+  .catch((err) => {
+    console.log('Error on database connection');
+    console.error(err);
+  });
 
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}...`);
