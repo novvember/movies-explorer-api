29969626d5e403a33ff811f92467009bf25701cf
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-05 19:03:42 +0300

    feat: add joi validation for delete movie

--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 const { getMovies, saveMovie, deleteMovie } = require('../controllers/movies');
-const { movieDataValidator } = require('../utils/validators');
+const {
+  movieDataValidator,
+  deleteMovieValidator,
+} = require('../utils/validators');
 
 const movies = express.Router();
 
 movies.get('/', getMovies);
 movies.post('/', movieDataValidator, saveMovie);
-movies.delete('/:movieId', deleteMovie);
+movies.delete('/:movieId', deleteMovieValidator, deleteMovie);
 
 module.exports = { movies };
--- utils/validators/index.js
+++ utils/validators/index.js
@@ -2,10 +2,12 @@ const { userInfoValidator } = require('./userInfoValidator');
 const { loginValidator } = require('./loginValidator');
 const { editableUserInfoValidator } = require('./editableUserInfoValidator');
 const { movieDataValidator } = require('./movieDataValidator');
+const { deleteMovieValidator } = require('./deleteMovieValidator');
 
 module.exports = {
   userInfoValidator,
   loginValidator,
   editableUserInfoValidator,
   movieDataValidator,
+  deleteMovieValidator,
 };
