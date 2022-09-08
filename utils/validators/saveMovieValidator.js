c75bb8c539323eadf4b0227c5710afffae8feeb2
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-08 11:36:53 +0300

    fix: app proper link validation with validator package

--- utils/validators/saveMovieValidator.js
+++ utils/validators/saveMovieValidator.js
@@ -1,4 +1,5 @@
 const { celebrate, Joi } = require('celebrate');
+const { validateUrl } = require('../validateUrl');
 
 const saveMovieValidator = celebrate({
   body: Joi.object().keys({
@@ -7,9 +8,9 @@ const saveMovieValidator = celebrate({
     duration: Joi.number().required(),
     year: Joi.string().required(),
     description: Joi.string().required(),
-    image: Joi.string().required().uri(),
-    trailerLink: Joi.string().required().uri(),
-    thumbnail: Joi.string().required().uri(),
+    image: Joi.string().required().custom(validateUrl),
+    trailerLink: Joi.string().required().custom(validateUrl),
+    thumbnail: Joi.string().required().custom(validateUrl),
     nameRU: Joi.string().required(),
     nameEN: Joi.string().required(),
     movieId: Joi.number().required(),
