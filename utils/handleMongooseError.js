e0752a712cab449ce58545f2a5e386b8d6f0bc74
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-08 11:40:39 +0300

    refactor: change method of mongoose validation error detection

--- utils/handleMongooseError.js
+++ utils/handleMongooseError.js
@@ -1,12 +1,8 @@
-const mongoose = require('mongoose');
 const { ValidationError } = require('../errors');
 const constants = require('./constants');
 
 function handleMongooseError(err) {
-  if (
-    err instanceof mongoose.Error.ValidationError
-    || err instanceof mongoose.Error.CastError
-  ) {
+  if (err.name === 'CastError' || err.name === 'ValidationError') {
     const fieldName = Object.keys(err.errors)[0];
     return new ValidationError(
       `${constants.validationError.MESSAGE_DETAILED} ${fieldName}`,
